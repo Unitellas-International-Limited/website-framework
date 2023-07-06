631c817358b0dffd56525397002a58155ae8a0ae
unitellas <136075335+unitellas@users.noreply.github.com>
2023-07-06 20:25:24 +0000

    recalibration

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,16 +1,6 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
 
-import dynamic from "next/dynamic";
-
-const UnitellasLayout = dynamic(() => import('@/components/Layout').then(mod => mod), {
-  ssr: false
-});
-
 export default function App({ Component, pageProps }: AppProps) {
-  return (
-    <UnitellasLayout>
-      <Component {...pageProps} />
-    </UnitellasLayout>
-  )
+  return <Component {...pageProps} />
 }
