71b1eb756bea334af90d67d105946aacf646515e
Damilola Akinlade <damak.dev@gmail.com>
2023-09-10 02:20:51 +0100

    external colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,10 +7,20 @@ module.exports = {
   ],
   theme: {
     extend: {
+      colors: {
+        "uni-blue": "#3caaee",
+      },
       backgroundImage: {
+        // logo: "url('https://www.unitellas.com.ng/images/unitellas-logo.png')",
+        "home-hero":
+          "url('https://www.unitellas.com.ng/images/cloud-tech-bg-3.jpg')",
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      },
+      fontFamily: {
+        display: ["Mongoose", "Arial", "sans-serif", "system-ui"],
+        // body: ["Inter", "Arial", "sans-serif", "system-ui"],
       },
     },
   },
