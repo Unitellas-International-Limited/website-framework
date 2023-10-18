2489391ddbe8fd39106b7b4dd8bd44316fe0a39e
unitellas <developer@unitellas.com>
2023-10-18 15:58:06 +0100

    favicon change

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -10,51 +10,85 @@ export default function Document() {
           href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;500;600;700&display=swap"
           rel="stylesheet"
         />
-        <meta name="description" content="Fully-managed enterprise edge cloud services - compute, networking, storage and more - designed for managed service providers and the modern enterprise." key="desc" />
-            <link rel="icon" type="image/x-icon" href="/favicon.ico" />
-            <link rel="shortcut icon" type="image/x-icon" href="/favicon.ico" />
-            <link rel="icon" type="image/png" sizes="32x32" href="/unitellasicon.png" />
-            <link rel="icon" type="image/png" sizes="16x16" href="/unitellasicon.png" />
-            <link rel='icon' sizes='192x192' type='image/png' href='/unitellasicon.png' />
-            <link rel='icon' sizes='512x512' type='image/png' href='/unitellasicon.png' />
-            <meta name="apple-mobile-web-app-title" content="Unitellas - THE FIRST HYPER SCALE EDGE CLOUD IN AFRICA" />
-            <link rel='apple-touch-icon' type='image/png' href='/unitellasicon.png' />
-            <meta name='msapplication-square310x310logo' content='/unitellasicon.png' />
-            <link rel='manifest' href='/' />
-            <link rel='canonical' href='/' />
+        <meta
+          name="description"
+          content="Fully-managed enterprise edge cloud services - compute, networking, storage and more - designed for managed service providers and the modern enterprise."
+          key="desc"
+        />
+        <link rel="icon" type="image/x-icon" href="/favicon.ico" />
+        <link rel="shortcut icon" type="image/x-icon" href="/favicon.ico" />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/unitellasicon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/unitellasicon.png"
+        />
+        <link
+          rel="icon"
+          sizes="192x192"
+          type="image/png"
+          href="/unitellasicon.png"
+        />
+        <link
+          rel="icon"
+          sizes="512x512"
+          type="image/png"
+          href="/unitellasicon.png"
+        />
+        <meta
+          name="apple-mobile-web-app-title"
+          content="Unitellas - THE FIRST HYPER SCALE EDGE CLOUD IN AFRICA"
+        />
+        <link
+          rel="apple-touch-icon"
+          type="image/png"
+          href="/unitellasicon.png"
+        />
+        <meta
+          name="msapplication-square310x310logo"
+          content="/unitellasicon.png"
+        />
+        <link rel="manifest" href="/" />
+        <link rel="canonical" href="/" />
 
-             {/* open graph */}
-            <meta property="og:type" content="website" />
-            <meta property="og:site_name" content="Unitellas" />
-            <meta
-              property="og:title"
-              content="Unitellas - THE FIRST HYPER SCALE EDGE CLOUD IN AFRICA"
-              key="title"
-            />
-            <meta
-              property="og:description"
-              content="Fully-managed enterprise edge cloud services - compute, networking, storage and more - designed for managed service providers and the modern enterprise."
-            />
-            <meta property="og:image" content=".png" />
-            <meta property="og:image:type" content="image/png" />
-            <meta property="og:image:alt" content="Unitellas" />
-            <meta property="og:image:secure_url" content=".png" />
-            <meta property="og:url" content="/" />
+        {/* open graph */}
+        <meta property="og:type" content="website" />
+        <meta property="og:site_name" content="Unitellas" />
+        <meta
+          property="og:title"
+          content="Unitellas - THE FIRST HYPER SCALE EDGE CLOUD IN AFRICA"
+          key="title"
+        />
+        <meta
+          property="og:description"
+          content="Fully-managed enterprise edge cloud services - compute, networking, storage and more - designed for managed service providers and the modern enterprise."
+        />
+        <meta property="og:image" content=".png" />
+        <meta property="og:image:type" content="image/png" />
+        <meta property="og:image:alt" content="Unitellas" />
+        <meta property="og:image:secure_url" content=".png" />
+        <meta property="og:url" content="/" />
 
-                {/* twitter cards */}
-            <meta name="twitter:card" content="summary_large_image" />
-            <meta name="twitter:site" content="@Unitellasil" />
-            <meta name="twitter:creator" content="@Unitellasil" />
-            <meta
-              name="twitter:title"
-              content="Unitellas - THE FIRST HYPER SCALE EDGE CLOUD IN AFRICA"
-            />
-            <meta
-              name="twitter:description"
-              content="Fully-managed enterprise edge cloud services - compute, networking, storage and more - designed for managed service providers and the modern enterprise."
-            />
-            <meta name="twitter:image" content=".png" />
-            <meta name="twitter:image:alt" content="Unitellas" />
+        {/* twitter cards */}
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:site" content="@Unitellasil" />
+        <meta name="twitter:creator" content="@Unitellasil" />
+        <meta
+          name="twitter:title"
+          content="Unitellas - THE FIRST HYPER SCALE EDGE CLOUD IN AFRICA"
+        />
+        <meta
+          name="twitter:description"
+          content="Fully-managed enterprise edge cloud services - compute, networking, storage and more - designed for managed service providers and the modern enterprise."
+        />
+        <meta name="twitter:image" content=".png" />
+        <meta name="twitter:image:alt" content="Unitellas" />
       </Head>
       <body>
         <Main />
