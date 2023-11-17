9a71f665adfa37c9c0aa73f6ac44906cf1141a28
Damilola Akinlade <damak.dev@gmail.com>
2023-11-17 05:24:19 +0100

    configure toaster

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,18 @@
 import "@/styles/globals.scss";
 import type { AppProps } from "next/app";
+import { Toaster } from "react-hot-toast";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Component {...pageProps} />
+      <Toaster
+        position="bottom-right"
+        toastOptions={{
+          duration: 4000,
+          className: "toast",
+        }}
+      />
+    </>
+  );
 }
