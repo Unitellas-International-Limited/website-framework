399a4a209d4c9300d9175532a7710e4b45f5574e
Sharon <omuwaste@gmail.com>
2025-03-28 13:25:10 +0100

    add country and bandwidth fields to demo form

--- src/pages/api/demo.ts
+++ src/pages/api/demo.ts
@@ -7,12 +7,15 @@ interface IPayload {
   senderPhone: string;
   senderNotes: string;
   orgName: string;
+  senderCountry: string;
   driveType: string;
   os: string;
   publicIP: number;
   cpuNumber: number;
   ramSize: string;
   customRamSize: string;
+  bandwidth: string;
+  customBandwidth: string;
   ssdGbTb: string;
   storageType: string;
   storageAmount: number;
@@ -27,12 +30,15 @@ export default async function handler(
     senderEmail,
     senderPhone,
     orgName,
+    senderCountry,
     os,
     publicIP,
     driveType,
     cpuNumber,
     ramSize,
     customRamSize,
+    bandwidth,
+    customBandwidth,
     ssdGbTb,
     storageType,
     storageAmount,
@@ -42,8 +48,12 @@ export default async function handler(
   const transporter = nodemailer.createTransport({
     service: "gmail",
     auth: {
-      user: "unitellas.itss@gmail.com",
-      pass: "qflg srsm pkfs jzqf",
+      user: "unitellasintl@gmail.com",
+      pass: "awtn juks adro qwmr",
+
+      // ALTERNATIVE
+      // user: "unitellas.itss@gmail.com",
+      // pass: "qflg srsm pkfs jzqf",
     },
   });
 
@@ -54,8 +64,8 @@ export default async function handler(
       to: "sharon@unitellas.com",
       cc: "info@unitellas.com.ng",
       subject: `Demo Request from ${senderName}`,
-      text: `Sender Email: ${senderEmail} Sender Phone Number: ${senderPhone} Organization: ${orgName} Demo Specifications Operating System: ${os} CPU Size: ${cpuNumber} Ram Size: ${ramSize} Custom Ram Size: ${customRamSize}  Drive Type: ${driveType} Storage Type: ${storageType} Storage Size: ${storageAmount} ${ssdGbTb}  Number of IPs needed: ${publicIP} Notes: ${senderNotes}`,
-      html: `<p> Sender Email: ${senderEmail} <br/> Sender Phone Number: ${senderPhone}<br/> Organization: ${orgName} <br/> <br/><br/> <b>Demo Specifications</b> <br/> Operating System: ${os} <br/> CPU Size: ${cpuNumber} <br/> Ram Size: ${ramSize} <br/> Custom Ram Size: ${customRamSize} <br/> Drive Type: ${driveType}<br/> Storage Type: ${storageType} <br/> Storage Size: ${storageAmount} ${ssdGbTb} <br/> Number of IPs needed: ${publicIP}<br/> Notes: ${senderNotes} </p>`,
+      text: `Sender Email: ${senderEmail} Sender Phone Number: ${senderPhone} Organization: ${orgName} Country: ${senderCountry} Demo Specifications Operating System: ${os} CPU Size: ${cpuNumber} Ram Size: ${ramSize} Custom Ram Size: ${customRamSize} Bandwidth: ${bandwidth} Custom Bandwidth: ${customBandwidth}  Drive Type: ${driveType} Storage Type: ${storageType} Storage Size: ${storageAmount} ${ssdGbTb}  Number of IPs needed: ${publicIP} Notes: ${senderNotes}`,
+      html: `<p> Sender Email: ${senderEmail} <br/> Sender Phone Number: ${senderPhone}<br/> Country: ${senderCountry} <br/> Organization: ${orgName} <br/> <br/> <b>Demo Specifications</b> <br/> Operating System: ${os} <br/> CPU Size: ${cpuNumber} <br/> Ram Size: ${ramSize} <br/> Custom Ram Size: ${customRamSize} <br/> Bandwidth: ${bandwidth} <br/> Custom Bandwidth: ${customBandwidth} <br/> Drive Type: ${driveType} <br/> Storage Type: ${storageType} <br/> Storage Size: ${storageAmount} ${ssdGbTb} <br/> Number of IPs needed: ${publicIP}<br/> Notes: ${senderNotes} </p>`,
     });
 
     res.status(200).json({ status: "OK" });
